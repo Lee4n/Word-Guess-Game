6abb433c4b06af8bddc8a183a778a416e0b66c68
Lee Lawler <lawler_lee@icloud.com>
2019-12-28 17:07:47 -0600

    made game loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,17 +10,35 @@ var wordBank = [
 ];
 // choose a random word from the word bank
 var word = wordBank[Math.floor(Math.random() * wordBank.length)];
-document.getElementById("currentWord").innerHTML = word;
+
 // Set up the answer array with for loop
 var answer = [];
+var guessedLetter = [];
+var remain = 10;
 for (let i = 0; i < word.length; i++) {
     answer[i] = ["_"];
 }
 document.getElementById("answer").innerHTML = answer.join(" ");
+
 // The game loop
+document.onkeypress = function (keyObject) {
+    if (remain > 0) {
+        if (word.includes(keyObject.key)) {
+            for (let i = 0; i < word.length; i++) {
+                if (word[i] == keyObject.key) {
+                    answer[i] = keyObject.key;
+                    document.getElementById("answer").innerHTML = answer.join(" ");
+                }
+            }
+        } else {
+            if (!guessedLetter.includes(keyObject.key)) {
+                remain--;
+                document.getElementById("remain").innerHTML = remain;
 
-// Get a guess from the player
+                guessedLetter.push(keyObject.key);
+                document.getElementById("guessedLetter").innerHTML = guessedLetter;
+            }
+        }
+    }
+};
 
-// Exit the game loop
-
-// Update the game state with the guess
